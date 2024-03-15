aaa3148e4e7ec4aab96489b56f25945a098ec276
Vasiliy Khudobin <146813280+khudobinv@users.noreply.github.com>
2024-03-15 23:51:32 +0300

    Добавлен title на основную страницу

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
+import { languageState } from "../../store/languageStore";
+import { getTranslation } from "../../utils/getTranslation";
+
 const Home = () => {
-	return <></>;
+	const currentLanguage = useRecoilValue(languageState);
+	return (
+		<>
+			<title>{getTranslation("krasnodarTransport", currentLanguage)}</title>
+		</>
+	);
 };
 
 export default Home;
