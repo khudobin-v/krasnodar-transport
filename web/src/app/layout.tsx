49907eb78e662decc3f020d168647c6a6870ba63
Vasiliy Khudobin <146813280+khudobinv@users.noreply.github.com>
2024-03-15 16:11:31 +0300

    Создан NextJS проект

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({ subsets: ["latin"] });
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={manrope.className}>{children}</body>
+		</html>
+	);
+}
