789cec4609a84a3292482cfde9f94a20b1c4a579
Vasiliy Khudobin <146813280+khudobinv@users.noreply.github.com>
2024-03-16 00:21:54 +0300

    Начато создание информационной панели

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import InfoPanel from "@/components/UI/InfoPanel";
 import { useRecoilValue } from "recoil";
 import { languageState } from "../../store/languageStore";
 import { getTranslation } from "../../utils/getTranslation";
@@ -9,6 +10,14 @@ const Home = () => {
 	return (
 		<>
 			<title>{getTranslation("krasnodarTransport", currentLanguage)}</title>
+			<div className="p-2 flex">
+				<div className="grid grid-cols-4 w-full gap-4">
+					<div className="col-span-1">
+						<InfoPanel />
+					</div>
+					<div className="col-span-3">Что-то другое</div>
+				</div>
+			</div>
 		</>
 	);
 };
--- web/utils/translations.ts
+++ web/utils/translations.ts
@@ -5,6 +5,30 @@ export const translations: Translations = {
 		ru: "Краснодарский транспорт",
 		en: "Krasnodar transport",
 	},
+	guest: {
+		ru: "гость",
+		en: "guest",
+	},
+	year: {
+		ru: "год",
+		en: "year",
+	},
+	goodNight: {
+		ru: "доброй ночи",
+		en: "good night",
+	},
+	goodMorning: {
+		ru: "доброе утро",
+		en: "good morning",
+	},
+	goodAfternoon: {
+		ru: "добрый день",
+		en: "good afternoon",
+	},
+	goodEvening: {
+		ru: "добрый вечер",
+		en: "good evening",
+	},
 	aboutProjectFooter__developing: {
 		ru: "Данный веб-сервис разрабатывается",
 		en: "This web service is developed by",
@@ -16,10 +40,5 @@ export const translations: Translations = {
 	aboutProjectFooter__openProject: {
 		ru: "Открытый исходный код проекта доступен на",
 		en: "The project is open source on",
-	},
-
-	year: {
-		ru: "год",
-		en: "year",
 	},
 };
