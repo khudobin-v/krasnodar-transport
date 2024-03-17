8d0d14d30ad5212cbf19fffbf0915f8a7ad14484
Vasiliy Khudobin <146813280+khudobinv@users.noreply.github.com>
2024-03-17 22:26:34 +0300

    Небольшие изменения в транспортной панели

--- web/src/components/UI/TransportPanel.tsx
+++ web/src/components/UI/TransportPanel.tsx
@@ -5,19 +5,19 @@ const TransportPanel = () => {
 		<div className='w-full grid grid-cols-3 gap-4 h-32 text-white'>
 			<Link
 				href='/tramRoutes'
-				className='bg-red-600 rounded-xl text-2xl font-bold relative'
+				className='bg-red-600 rounded-xl text-2xl font-bold relative hover:bg-red-700 transition'
 			>
 				<h3 className='absolute right-5 top-5'>Трамвай</h3>
 			</Link>
 			<Link
 				href='/trolleybusRoutes'
-				className='bg-blue-600 rounded-xl text-2xl font-bold relative'
+				className='bg-blue-600 rounded-xl text-2xl font-bold relative hover:bg-blue-700 transition'
 			>
 				<h3 className='absolute right-5 top-5'>Троллейбус</h3>
 			</Link>
 			<Link
 				href='/busRoutes'
-				className='bg-green-600 rounded-xl text-2xl font-bold relative'
+				className='bg-green-600 rounded-xl text-2xl font-bold relative hover:bg-green-700 transition'
 			>
 				<h3 className='absolute right-5 top-5'>Автобус</h3>
 			</Link>
